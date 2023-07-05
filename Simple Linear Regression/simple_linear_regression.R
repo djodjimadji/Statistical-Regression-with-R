c75923d575706d39605eb29a2becb6c91a39cf01
Alexis DJODJIMADJI <115734760+djodjimadji@users.noreply.github.com>
2023-07-05 21:20:53 +0100

    Update simple_linear_regression.R

--- Simple Linear Regression/simple_linear_regression.R
+++ Simple Linear Regression/simple_linear_regression.R
@@ -4,12 +4,12 @@ data_simulated <- read.csv("data_simulated.csv")
 
 cor_comput<-function(x,y)
   {
-  n=length(x)
-  x_bar=mean(x)
-  y_bar=mean(y)
-  var_x=sum((x-x_bar)^2)/(n-1)
-  var_y=sum((y-y_bar)^2)/(n-1)
-  covar_x_y=sum((x-x_bar)*(y-y_bar))/(n-1)
-  cor_x_y=covar_x_y/(var_x*var_y)^0.5
+  n <- length(x)
+  x_bar <- mean(x)
+  y_bar <- mean(y)
+  var_x <- sum((x-x_bar)^2)/(n-1)
+  var_y <- sum((y-y_bar)^2)/(n-1)
+  covar_x_y <- sum((x-x_bar)*(y-y_bar))/(n-1)
+  cor_x_y <- covar_x_y/(var_x*var_y)^0.5
   return(cor_x_y)
 }
